e5adc791eae6506ab76d31a156cb50c35e461807
Jishitha <jishithanaidu21@gmail.com>
2025-08-05 10:32:24 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,11 @@
+package oops_practical;
+
+public class Circle {
+	double radius = 5.0;  
+    void Area() {
+        double area = 3.14159 * radius * radius;
+        System.out.println("Radius: " + radius);
+        System.out.println("Area of the circle: " + area);
+    }
+
+}
